5cfbb4263ce42c6950d3496a40be618dce6cb41f
Conway Ying <yingkongwei@gmail.com>
2017-12-17 17:53:38 +1100

    Main changes

--- process_video.py
+++ process_video.py
@@ -45,3 +45,21 @@ def main():
     elapsed_time = time.time() - start.time()
     print('Elapsed time (total): {:.2f}'.format(fps.elapsed()))
     print('Approx. FPS: {:.2f}'.format(fps.fps()))
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    # Make sure these files are in the root directory
+    # You can download a trained facial shape predictor and recognition model from:
+    # http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2
+    # http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
+    predictor_fn = "shape_predictor_5_face_landmarks.dat"
+    face_rec_model_path_fn = "dlib_face_recognition_resnet_model_v1.dat"
+
+    # Create a face detector
+    face_detector = dlib.get_frontal_face_detector()
+    sp = dlib.shape_predictor(predictor_fn)
+    facerec = dlib.face_recognition_model_v1(face_rec_model_path_fn)
+
+    main()
