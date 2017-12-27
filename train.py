7b43cd898e1c30ec0adf6a04d08691bfa6acc915
Conway Ying <conwayying@macs-mbp.home>
2017-12-27 16:46:49 +1100

    Refactor

--- train.py
+++ train.py
@@ -1,70 +1,102 @@
 import cv2
 import numpy
+import argparse
+from pathlib import Path
 
 from utils import get_image_paths, load_images, stack_images
 from training_data import get_training_data
 
-from model import autoencoder_A
-from model import autoencoder_B
-from model import encoder, decoder_A, decoder_B
+from model import Encoder, Decoder, Autoencoder
 
-try:
-    encoder  .load_weights( "models/encoder.h5"   )
-    decoder_A.load_weights( "models/decoder_A.h5" )
-    decoder_B.load_weights( "models/decoder_B.h5" )
-except:
-    pass
 
-def save_model_weights():
-    encoder  .save_weights( "models/encoder.h5"   )
-    decoder_A.save_weights( "models/decoder_A.h5" )
-    decoder_B.save_weights( "models/decoder_B.h5" )
-    print( "Saved model weights" )
+def main(FLAGS):
+    encoder = Encoder()
+    decoder_A = Decoder()
+    decoder_B = Decoder()
+    autoencoder_A = Autoencoder(encoder, decoder_A)
+    autoencoder_B = Autoencoder(encoder, decoder_B)
 
-images_A = get_image_paths( "training_data/trump" )
-images_B = get_image_paths( "training_data/cage"  )
-images_A = load_images( images_A ) / 255.0
-images_B = load_images( images_B ) / 255.0
+    # create paths
+    models_dir = Path("models")
+    training_data_dir = Path("data/training_data")
+    person_A_training_data = training_data_dir / Path(FLAGS.person_A)
+    person_B_training_data = training_data_dir / Path(FLAGS.person_B)
+    encoder_fn = models_dir / Path("encoder" + ".h5")
+    decoder_A_fn = models_dir / Path("decoder_" + FLAGS.person_A + ".h5")
+    decoder_B_fn = models_dir / Path("decoder_" + FLAGS.person_B + ".h5")
 
-images_A += images_B.mean( axis=(0,1,2) ) - images_A.mean(axis=(0,1,2) )
+    try:
+        encoder.load_weights(str(encoder_fn))
+    except IOError as err:
+        user_input = input(
+            "Couldn't load encoder. Do you want to create a new one? (Y/n)\n")
+        if user_input is not "Y":
+            exit(1)
 
-print( "Press 'q' to stop training and save model" )
+    try:
+        decoder_A.load_weights(str(decoder_A_fn))
+        decoder_B.load_weights(str(decoder_B_fn))
+    except:
+        user_input = input(
+            "Couldn't load decoder/s. Do you want to create a new one? (Y/n)\n")
+        if user_input is not "Y":
+            exit(1)
 
-for iteration in range(1000000):
-    batch_size = 64
-    warped_A, target_A = get_training_data( images_A, batch_size )
-    warped_B, target_B = get_training_data( images_B, batch_size )
+    def save_model_weights():
+        encoder  .save_weights(str(encoder_fn))
+        decoder_A.save_weights(str(decoder_A_fn))
+        decoder_B.save_weights(str(decoder_B_fn))
+        print("Saved model weights")
 
-    loss_A = autoencoder_A.train_on_batch( warped_A, target_A )
-    loss_B = autoencoder_B.train_on_batch( warped_B, target_B )
-    print("Iteration {}".format(iteration)
-    print("autoencoder_A loss: {} | autoencoder_B loss: {}".format(loss_A, loss_B))
+    images_A = get_image_paths(str(person_A_training_data))
+    images_B = get_image_paths(str(person_B_training_data))
+    images_A = load_images(images_A) / 255.0
+    images_B = load_images(images_B) / 255.0
 
-    if iteration % 100 == 0:
-        save_model_weights()
-        test_A = target_A[0:14]
-        test_B = target_B[0:14]
+    print("Press 'q' to stop training and save model")
 
-    figure_A = numpy.stack([
-        test_A,
-        autoencoder_A.predict( test_A ),
-        autoencoder_B.predict( test_A ),
-        ], axis=1 )
-    figure_B = numpy.stack([
-        test_B,
-        autoencoder_B.predict( test_B ),
-        autoencoder_A.predict( test_B ),
-        ], axis=1 )
+    for iteration in range(1000000):
+        batch_size = 64
+        warped_A, target_A = get_training_data(images_A, batch_size)
+        warped_B, target_B = get_training_data(images_B, batch_size)
 
-    figure = numpy.concatenate( [ figure_A, figure_B ], axis=0 )
-    figure = figure.reshape( (4,7) + figure.shape[1:] )
-    figure = stack_images( figure )
+        loss_A = autoencoder_A.train_on_batch(warped_A, target_A)
+        loss_B = autoencoder_B.train_on_batch(warped_B, target_B)
+        print("autoencoder_A loss: {} | autoencoder_B loss: {}".format(loss_A, loss_B))
 
-    figure = numpy.clip( figure * 255, 0, 255 ).astype('uint8')
+        if iteration % 5 == 0:
+            save_model_weights()
+            test_A = target_A[0:14]
+            test_B = target_B[0:14]
 
-    cv2.imshow( "", figure )
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        save_model_weights()
-        exit()
+            figure_A = numpy.stack([
+                test_A,
+                autoencoder_A.predict(test_A),
+                autoencoder_B.predict(test_A),
+            ], axis=1)
+            figure_B = numpy.stack([
+                test_B,
+                autoencoder_B.predict(test_B),
+                autoencoder_A.predict(test_B),
+            ], axis=1)
 
+            figure = numpy.concatenate([figure_A, figure_B], axis=0)
+            figure = figure.reshape((4, 7) + figure.shape[1:])
+            figure = stack_images(figure)
+
+            figure = numpy.clip(figure * 255, 0, 255).astype('uint8')
+
+            cv2.imshow("", figure)
+            key = cv2.waitKey(1)
+
+        if key == ord('q'):
+            save_model_weights()
+            exit()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("person_A", type=str)
+    parser.add_argument("person_B", type=str)
+    FLAGS, unparsed = parser.parse_known_args()
+    main(FLAGS)
